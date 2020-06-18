1113f2358ebb0e131a08128c0a8efcfe101d50bf
Jacob Tepperman <jtepp@icloud.com>
2020-06-18 13:40:36 -0400

    2.0 Overhaul

--- script.js
+++ script.js
@@ -1,3 +1,40 @@
+class Definition {
+    constructor(parts, deff) {
+        this.parts = parts;
+        this.deff = deff;
+    }
+}
+class Curd {
+    constructor(word, score, p, d, s, f, r) {
+        this.word = word;
+        this.score = score;
+        this.p = p;
+        this.d = d;
+        this.s = s;
+        this.f = f;
+        this.r = r;
+    }
+
+}
+var curds = {};
+var currFilt = "All";
+const filter = document.getElementById('filter');
+availParts = [];
+party = {
+    'n': 'Noun',
+    'adj': 'Adjective',
+    'a': 'Adjective',
+    'adv': 'Adverb',
+    'v': 'Verb',
+    'pro': 'Pronoun',
+    'pron': 'Pronoun',
+    'pre': 'Preposition',
+    'prep': 'Preposition',
+    'in': 'Interjection',
+    'int': 'Interjection',
+    'con': 'Conjunction',
+    'conj': 'Conjunction'
+}
 const pre = {
     "Means like": "ml",
     "Sounds like": "sl",
@@ -19,7 +56,6 @@ const pre = {
     "Homophone": "rel_hom",
     "Consonant match": "rel_cns",
 };
-
 var starter = '';
 var ender = '';
 const pro = {
@@ -29,15 +65,15 @@ const pro = {
 };
 
 const mmdd = {
-    "Definition": "d",
-    "Part of speech": "p",
-    "Syllable count": "s",
     "Pronunciation": "r",
-    "Word frequency": "f",
+    // "Part of speech": "p",
+    "Syllable count": "s",
+    "Definitions": "d",
+    // "Word frequency": "f",
 };
-const ta = document.getElementById("textarea");
+const cont = document.getElementsByClassName('container')[0];
 const ff = document.getElementById('first');
-const base = "https://api.datamuse.com/words?";
+const base = "http://api.datamuse.com/words?";
 var bonus = '';
 var terms = 1;
 var anyChecked = false;
@@ -64,21 +100,30 @@ for (m in mmdd) {
     b.setAttribute('id', m);
     b.setAttribute("value", m);
     md.appendChild(b);
-    md.innerHTML += "&nbsp" + m;
-    md.innerHTML += "<br>";
+    const l = document.createElement('label');
+    l.setAttribute('for', m);
+    l.innerHTML = m;
+    md.appendChild(l);
+    md.appendChild(document.createElement('br'));
 }
 
 
 document.getElementById("submit").onclick = () => {
+    currFilt = "All";
+    document.getElementById('seive').setAttribute('style', 'opacity: 1;');
+    cont.innerHTML = "";
     starter = '';
     ender = '';
+    // document.getElementById('seive').setAttribute('style', 'opacity: 0;');
     if (document.getElementById("t0").value.length > 0) {
         var url = base;
 
         for (var i = 0; i < Object.keys(selects).length; i++) {
-            if (i != 0) url += "&";
             const temp = selects['s' + i].options[selects['s' + i].selectedIndex].value;
-            if (temp != 'en' && temp != 'st') { url += temp + '=' + replaceAll(texts['t' + i].value, ' ', '+'); }
+            if (temp != 'en' && temp != 'st' && texts['t' + i].value != '') {
+                if (i != 0) url += "&";
+                url += temp + '=' + replaceAll(texts['t' + i].value, ' ', '+');
+            }
             else if (temp == 'st') { starter += replaceAll(texts['t' + i].value, ' ', '+'); }
             else if (temp == 'en') { ender += replaceAll(texts['t' + i].value, ' ', '+'); }
 
@@ -89,29 +134,213 @@ document.getElementById("submit").onclick = () => {
                 url += '&' + pro[ro] + '=' + replaceAll(document.getElementById(pro[ro]).value, ' ', '+');
         }
         for (m in mmdd) {
-            if (document.getElementById(m).checked) anyChecked = true;
+            if (document.getElementById(m).checked && m != 'Pronunciation') anyChecked = true;
         }
-        if (anyChecked) url += '&md=';
-        for (m in mmdd) {
-            if (document.getElementById(m).checked) url += mmdd[m];
-        }
+        url += '&md=pds';
+        url += 'fr&ipa=1'
         if (max.value != 0) url += "&max=" + max.value;
         console.log(url);
         dog(url);
+    } else {
+        // console.log(false);
+        document.getElementById('seive').setAttribute('style', 'opacity: 0;');
+        cont.innerHTML = "<div style='position: fixed; top: 100px; right:20vw;'>Please provide a search term</div>"
+        // document.creat
     }
+
 };
 
 async function dog(link) {
+
+
+    curds = {};
+    availParts = [];
+    var filterPass = false;
     var r;
+    var part = {};
+    var pron = {};
+    var fre = {};
+    var syls = {};
+    var defs = {};
     await fetch(link).then(r => r.json()).then(d => { r = d; })
+
     for (o of r) {
-        var cc = 0;
-        for (oo in o) { ta.innerHTML += Object.keys(o)[cc] + ': ' + o[oo] + "\n"; cc++ }
-        ta.innerHTML += "\n\n";
+        const ww = o['word'];
+        if (o['defs'] == undefined || o['defs'] == null || o['defs'] == '' || o['defs'].length < 1) o['defs'] = ["n/a	[No definition available]"];
+        const tempArray = o['defs'].toString().split(',');
+        defs[o] = new Definition([], []);
+
+        for (a of tempArray) {
+            if (a.split('	')[0][0] != ' ' && a.split('	')[0][0] != null && a.split('	')[0][0] != '' && a.split('	')[0][0] != 'undefined' && a.split('	')[0][0] != undefined) {
+                defs[o].parts.push(party[a.split('	')[0][0]]);
+                defs[o].deff.push(a.split('	')[1]);
+                if (!availParts.includes(party[a.split('	')[0][0]])) availParts.push(party[a.split('	')[0][0]]);
+            }
+
+        }
+
+        pron[o] = o['tags'].slice(0, o['tags'].indexOf('pron:')).pop().split(':')[1];
+        var ugh = o['tags'].slice(0, o['tags'].indexOf('pron:'));
+        ugh.pop();
+        ugh.pop();
+        part[o] = ugh;
+        fre[o] = o['tags'][o['tags'].length - 1].split(':')[1];
+        syls[o] = o['numSyllables'];
+        // console.log(o['tags']);
+        // console.log(part[o]);
+        curds[ww] = new Curd(o['word'], o['score'], defs[o].parts, defs[o].deff, syls[o], fre[o], pron[o]);
+
+
+        // var cc = 0;
+        const cd = document.createElement('div');
+        cd.setAttribute('class', 'card');
+        cd.setAttribute('id', curds[ww].score);
+        cd.setAttribute('onmousedown', 'copy("' + capitalize(curds[ww].word) + '", ' + curds[ww].score + ')');
+        const h = document.createElement('h1');
+        h.setAttribute('id', capitalize(curds[ww].word))
+        h.innerHTML = capitalize(curds[ww].word);
+
+        const sup = document.createElement('sup');
+        sup.innerHTML = 'Accuracy: ' + curds[ww].score;
+        h.appendChild(sup);
+
+        const har = document.createElement('hr');
+        har.setAttribute('class', 'har');
+
+        const p = document.createElement('p');
+        // delete o['word'];
+        // delete o['score'];
+        // delete o['tags'];
+        // delete o['numSyllables'];
+        // delete o['numSyllables'];
+        // for (oo in o) { p.innerHTML += Object.keys(o)[cc] + ': ' + o[oo] + "<br>"; cc++; }
+        if (document.getElementById('Pronunciation').checked) p.innerHTML += '<b>Pronunciation:</b><br> ' + curds[ww].r + '<br>';
+        if (document.getElementById('Syllable count').checked) p.innerHTML += '<b>Syllable count:</b> ' + curds[ww].s + '<br>';
+        if (document.getElementById('Definitions').checked && o['defs'] != "n/a	[No definition available]") {
+            p.innerHTML += '<b>Definitions:</b><br>';
+            for (let i = 0; i < curds[ww].d.length; i++) {
+                p.innerHTML += `[${curds[ww].p[i]}] ${capitalize(curds[ww].d[i])}. <br><br>`;
+            }
+            p.innerHTML += '<br>';
+        }
+
+        const sub = document.createElement('sub');
+        sub.innerHTML = 'Frequency: ' + curds[ww].f;
+        // console.log(p.innerHTML);
+        cd.appendChild(h);
+        if (p.innerHTML.length > 0) cd.appendChild(har);
+        cd.appendChild(p);
+        cd.appendChild(sub);
+
+        if (currFilt == "All") filterPass = true;
+        else if (Object.values(party).includes(currFilt) && curds[ww].p.includes(currFilt)) {
+            filterPass = true;
+        }
+        else filterPass = false;
+        if (filterPass) { cont.appendChild(cd); }
+
+
+
+        // for (oo in o) { ta.innerHTML += Object.keys(o)[cc] + ': ' + o[oo] + "\n"; cc++ }
+        // ta.innerHTML += "\n\n";
+    }
+    if (!availParts.includes("All")) availParts.push("All");
+
+    if (availParts.length < 2 || cont.innerHTML == "") { document.getElementById('seive').setAttribute('style', 'opacity: 0;'); cont.innerHTML = "<div style='position: fixed; top: 100px; right:20vw;'>No Results :(</div>" }
+    filter.innerHTML = '';
+    for (pp of availParts) {
+        const l = document.createElement('label');
+        l.setAttribute('for', pp);
+        l.innerHTML = pp;
+        const r = document.createElement('input');
+        r.setAttribute('type', 'radio');
+        r.setAttribute('name', 'filth');
+        r.setAttribute('id', pp)
+        r.setAttribute('value', pp);
+        r.setAttribute('checked', 'true');
+        r.setAttribute('onclick', 'fille("' + pp + '")')
+        filter.appendChild(r);
+        filter.appendChild(l);
     }
 
-}
 
+
+
+}
+function fille(input) {
+    currFilt = input;
+
+    cont.innerHTML = "";
+    for (ww in curds) {
+        const cd = document.createElement('div');
+        cd.setAttribute('class', 'card');
+        cd.setAttribute('id', curds[ww].score);
+        cd.setAttribute('onmousedown', 'copy("' + capitalize(curds[ww].word) + '", ' + curds[ww].score + ')');
+        const h = document.createElement('h1');
+        h.setAttribute('id', capitalize(curds[ww].word))
+        h.innerHTML = capitalize(curds[ww].word);
+
+        const sup = document.createElement('sup');
+        sup.innerHTML = 'Accuracy: ' + curds[ww].score;
+        h.appendChild(sup);
+
+        const har = document.createElement('hr');
+        har.setAttribute('class', 'har');
+
+        const p = document.createElement('p');
+        // delete o['word'];
+        // delete o['score'];
+        // delete o['tags'];
+        // delete o['numSyllables'];
+        // delete o['numSyllables'];
+        // for (oo in o) { p.innerHTML += Object.keys(o)[cc] + ': ' + o[oo] + "<br>"; cc++; }
+        if (document.getElementById('Pronunciation').checked) p.innerHTML += '<b>Pronunciation:</b> ' + curds[ww].r + '<br>';
+        if (document.getElementById('Syllable count').checked) p.innerHTML += '<b>Syllable count:</b> ' + curds[ww].s + '<br>';
+        if (document.getElementById('Definitions').checked && o['defs'] != "n/a	[No definition available]") {
+            p.innerHTML += '<b>Definitions:</b><br>';
+            for (let i = 0; i < curds[ww].d.length; i++) {
+                p.innerHTML += `[${curds[ww].p[i]}] ${capitalize(curds[ww].d[i])}. <br><br>`;
+            }
+            p.innerHTML += '<br>';
+        }
+
+        const sub = document.createElement('sub');
+        sub.innerHTML = 'Frequency: ' + curds[ww].f;
+        // console.log(p.innerHTML);
+        cd.appendChild(h);
+        if (p.innerHTML.length > 0) cd.appendChild(har);
+        cd.appendChild(p);
+        cd.appendChild(sub);
+
+        if (currFilt == "All") filterPass = true;
+        else if (Object.values(party).includes(currFilt) && curds[ww].p.includes(currFilt)) {
+            filterPass = true;
+        }
+        else filterPass = false;
+        if (filterPass) { cont.appendChild(cd); }
+    }
+
+
+}
+function copy(id, score) {
+    const clip = document.getElementById('clip');
+    const dd = document.getElementById(score);
+    document.getElementById('clip').style.opacity = 1;
+    dd.style.background = "green";
+    const el = document.createElement('textarea');
+    el.value = document.getElementById(id).innerHTML.split('<')[0];
+    document.body.appendChild(el);
+    el.select();
+    document.execCommand('copy');
+    document.body.removeChild(el);
+    setTimeout(function () {
+
+        document.getElementById('clip').style.opacity = 0;
+        dd.style.background = "none";
+    }, 800);
+
+
+}
 
 
 function add() {
@@ -152,7 +381,7 @@ function add() {
 document.body.addEventListener("keypress", function (event) {
     if (event.keyCode === 13) {
         event.preventDefault();
-        ta.innerHTML = "";
+
         document.getElementById("submit").click();
     }
 });
@@ -165,3 +394,8 @@ function replaceAll(tat, s, r) {
     }
     return sss.trim();
 };
+
+function capitalize(s) {
+    if (typeof s !== 'string') return ''
+    return (s.charAt(0).toUpperCase() + s.slice(1)).trim()
+}
